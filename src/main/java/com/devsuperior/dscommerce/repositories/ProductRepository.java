084cc1fec14f9b416081442d459172223407018d
Weslley Palhares <weslley_palhares@outlook.com>
2025-02-01 17:20:17 -0300

    GET product by id

---
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+
+}
