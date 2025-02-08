136e48f8e7f7ecfabed2407ddf1e2349a1a51f83
Weslley Palhares <weslley_palhares@outlook.com>
2025-02-08 00:53:34 -0300

    ProductDTO with categories

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscommerce.dto.CategoryDTO;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.dto.ProductMinDTO;
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
@@ -80,6 +82,13 @@ public class ProductService {
 			entity.setDescription(dto.getDescription());
 			entity.setPrice(dto.getPrice());
 			entity.setImgUrl(dto.getImgUrl());
+			
+			entity.getCategories().clear();
+			for(CategoryDTO catDTO : dto.getCategories()) {
+				Category cat = new Category();
+				cat.setId(catDTO.getId());
+				entity.getCategories().add(cat);
+			}
 		}
 		
 		
