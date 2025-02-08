51a8fd8376be0a0a17d20a006c0d8f90ca69c144
Weslley Palhares <weslley_palhares@outlook.com>
2025-02-08 02:06:22 -0300

    imgUrl OrderItemDTO

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,13 +8,15 @@ public class OrderItemDTO {
 	private String name;
 	private Double price;
 	private Integer quantity;
+	private String imgUrl;
 	
 	
-	public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+	public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
 		this.productId = productId;
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+		this.imgUrl = imgUrl;
 	}
 	
 	public OrderItemDTO(OrderItem entity) {
@@ -22,6 +24,8 @@ public class OrderItemDTO {
 		name = entity.getProduct().getName();
 		price = entity.getPrice();
 		quantity = entity.getQuantity();
+		imgUrl = entity.getProduct().getImgUrl();
+		
 	}
 
 	public Long getProductId() {
@@ -45,4 +49,8 @@ public class OrderItemDTO {
 		return price * quantity;
 	}
 	
+	
+	public String getImgUrl() {
+		return imgUrl;
+	}
 }
